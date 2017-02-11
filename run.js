795e122ea8f47242c3e2ef158e38d83c4573a0ac
Joseph Kahn <josephkahn@college.harvard.edu>
2017-02-11 14:27:28 -0500

    remove firebase publish

--- run.js
+++ run.js
@@ -18,9 +18,8 @@ const webpack = require('webpack');
 
 // TODO: Update configuration settings
 const config = {
-  title: 'React Static Boilerplate',        // Your website title
-  url: 'https://rsb.kriasoft.com',          // Your website URL
-  project: 'react-static-boilerplate',      // Firebase project. See README.md -> How to Deploy
+  title: 'Tabula',        // Your website title
+  url: 'https://www.tabula.life',          // Your website URL
   trackingID: 'UA-XXXXX-Y',                 // Google Analytics Site's ID
 };
 
@@ -97,14 +96,43 @@ tasks.set('build', () => {
 // Build and publish the website
 // -----------------------------------------------------------------------------
 tasks.set('publish', () => {
-  const firebase = require('firebase-tools');
-  return run('build')
-    .then(() => firebase.login({ nonInteractive: false }))
-    .then(() => firebase.deploy({
-      project: config.project,
-      cwd: __dirname,
-    }))
-    .then(() => { setTimeout(() => process.exit()); });
+  const remote = {
+    url: 'https://github.com/josephwandile/tabula.git',
+    branch: 'gh-pages',
+  };
+  global.DEBUG = process.argv.includes('--debug') || false;
+  const spawn = require('child_process').spawn;
+  const opts = { cwd: path.resolve(__dirname, './build'), stdio: ['ignore', 'inherit', 'inherit'] };
+  const git = (...args) => new Promise((resolve, reject) => {
+    spawn('git', args, opts).on('close', code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`git ${args.join(' ')} => ${code} (error)`));
+      }
+    });
+  });
+
+  return Promise.resolve()
+    .then(() => run('clean'))
+    .then(() => git('init', '--quiet'))
+    .then(() => git('config', '--get', 'remote.origin.url')
+      .then(() => git('remote', 'set-url', 'origin', remote.url))
+      .catch(() => git('remote', 'add', 'origin', remote.url))
+    )
+    .then(() => git('ls-remote', '--exit-code', remote.url, 'master')
+      .then(() => Promise.resolve()
+        .then(() => git('fetch', 'origin'))
+        .then(() => git('reset', `origin/${remote.branch}`, '--hard'))
+        .then(() => git('clean', '--force'))
+      )
+      .catch(() => Promise.resolve())
+    )
+    .then(() => run('build'))
+    .then(() => git('add', '.', '--all'))
+    .then(() => git('commit', '--message', new Date().toUTCString())
+      .catch(() => Promise.resolve()))
+    .then(() => git('push', 'origin', `HEAD:${remote.branch}`, '--force', '--set-upstream'));
 });
 
 //
@@ -149,46 +177,6 @@ tasks.set('start', () => {
       }
     });
   }));
-});
-
-tasks.set('publish', () => {
-  const remote = {
-    url: 'https://github.com/josephwandile/tabula.git',
-    branch: 'gh-pages',
-  };
-  global.DEBUG = process.argv.includes('--debug') || false;
-  const spawn = require('child_process').spawn;
-  const opts = { cwd: path.resolve(__dirname, './build'), stdio: ['ignore', 'inherit', 'inherit'] };
-  const git = (...args) => new Promise((resolve, reject) => {
-    spawn('git', args, opts).on('close', code => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`git ${args.join(' ')} => ${code} (error)`));
-      }
-    });
-  });
-
-  return Promise.resolve()
-    .then(() => run('clean'))
-    .then(() => git('init', '--quiet'))
-    .then(() => git('config', '--get', 'remote.origin.url')
-      .then(() => git('remote', 'set-url', 'origin', remote.url))
-      .catch(() => git('remote', 'add', 'origin', remote.url))
-    )
-    .then(() => git('ls-remote', '--exit-code', remote.url, 'master')
-      .then(() => Promise.resolve()
-        .then(() => git('fetch', 'origin'))
-        .then(() => git('reset', `origin/${remote.branch}`, '--hard'))
-        .then(() => git('clean', '--force'))
-      )
-      .catch(() => Promise.resolve())
-    )
-    .then(() => run('build'))
-    .then(() => git('add', '.', '--all'))
-    .then(() => git('commit', '--message', new Date().toUTCString())
-      .catch(() => Promise.resolve()))
-    .then(() => git('push', 'origin', `HEAD:${remote.branch}`, '--force', '--set-upstream'));
 });
 
 // Execute the specified task or default one. E.g.: node run build
